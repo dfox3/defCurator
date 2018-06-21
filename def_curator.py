f540b1432cd8f0559c79be9e00b1e2236912218b
U-PERKINELMERNET\FoxDJ <fox.dylan92@gmail.com>
2018-06-21 13:56:03 -0500

    added the dang ol script

---
+++ def_curator.py
@@ -0,0 +1,163 @@
+import os
+import argparse
+from datetime import datetime
+
+#--------------------------------------------------------------------------------------------------
+#Command line input parameters
+parser = argparse.ArgumentParser(description='Definition Curator')
+
+parser.add_argument('--i',
+                    metavar='-input',
+                    required=True,
+                    help='input python script')
+#--------------------------------------------------------------------------------------------------
+
+def main():
+    start_time = datetime.now()
+    options = parser.parse_args()
+
+    print_out = "import os\n\n"
+    print_out += "#--------------------------------------------------------------------------------------------------\n"
+    print_out += "# def curator; written by dylan fox 20180621; version 1.0.0.0\n"
+    print_out += "#--------------------------------------------------------------------------------------------------\n\n"
+    print_out += "#ex:\n"
+    print_out += "#\tdefName(input1,input2,...,inputN)\n"
+    print_out += "#\t\t#return output\n\n"
+    print_out += "#--------------------------------------------------------------------------------------------------\n\n"   
+    print_out += "def main():\n"
+
+    def_found = False
+    class_found = False
+    class_def_found = False
+    with open(options.i) as f:
+        content = f.readlines()
+        for c in content:
+            if def_found:
+                if c[0:10] == "    return":
+                    print_out += "\t\t#" + str(c[4:]) + "\n"
+                    def_found = False
+                
+                if c[0:3] == "def":
+
+                    print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                    print_out += "\ttitle: " + str(c[4:].split("(")[0]) + "\n"
+                    print_out += "\tfunction:\n"
+                    print_out += "\tinput:\n"
+                    print_out += "\toutput:\n"
+                    print_out += "\t\'\'\'\n"
+                    print_out += "\t" + str(c[4:].split(":")[0]) + "\n"
+                elif c[0:5] == "class":
+                    class_found = True
+                    def_found = False
+                    class_def_found = False
+                    print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                    print_out += "\ttitle: " + str(c[6:].split(":")[0].split("(")[0]) + "\n"
+                    print_out += "\tfunction:\n"
+                    print_out += "\tinput:\n"
+                    print_out += "\toutput:\n"
+                    print_out += "\t\'\'\'\n"
+                    print_out += "\t" + str(c[6:].split(":")[0]) + " #class\n"
+
+            elif class_found:
+                if class_def_found:
+                    if c[0:14] == "        return":
+                        print_out += "\t\t\t#" + str(c[8:]) + "\n"
+                        class_def_found = False
+                    if c[0:7] == "    def":
+                        print_out += "\n\n\t\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                        print_out += "\t\ttitle: " + str(c[8:].split("(")[0]) + "\n"
+                        print_out += "\t\tfunction:\n"
+                        print_out += "\t\tinput:\n"
+                        print_out += "\t\toutput:\n"
+                        print_out += "\t\t\'\'\'\n"
+                        print_out += "\t\t#" + str(c[8:].split(":")[0]) + "\n"
+                    if c[0:3] == "def":
+
+                        print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                        print_out += "\ttitle: " + str(c[4:].split("(")[0]) + "\n"
+                        print_out += "\tfunction:\n"
+                        print_out += "\tinput:\n"
+                        print_out += "\toutput:\n"
+                        print_out += "\t\'\'\'\n"
+                        print_out += "\t" + str(c[4:].split(":")[0]) + "\n"
+                        class_def_found = False
+                        def_found = True
+                        class_found = False
+                    elif c[0:5] == "class":
+                        class_found = True
+                        def_found = False
+                        class_def_found = False
+                        print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                        print_out += "\ttitle: " + str(c[6:].split(":")[0].split("(")[0]) + "\n"
+                        print_out += "\tfunction:\n"
+                        print_out += "\tinput:\n"
+                        print_out += "\toutput:\n"
+                        print_out += "\t\'\'\'\n"
+                        print_out += "\t" + str(c[6:].split(":")[0]) + " #class\n"
+                else:
+                    if c[0:7] == "    def":
+                        class_def_found = True
+                        print_out += "\n\n\t\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                        print_out += "\t\ttitle: " + str(c[8:].split("(")[0]) + "\n"
+                        print_out += "\t\tfunction:\n"
+                        print_out += "\t\tinput:\n"
+                        print_out += "\t\toutput:\n"
+                        print_out += "\t\t\'\'\'\n"
+                        print_out += "\t\t#" + str(c[8:].split(":")[0]) + "\n"
+                    if c[0:3] == "def":
+                        print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                        print_out += "\ttitle: " + str(c[4:].split("(")[0]) + "\n"
+                        print_out += "\tfunction:\n"
+                        print_out += "\tinput:\n"
+                        print_out += "\toutput:\n"
+                        print_out += "\t\'\'\'\n"
+                        print_out += "\t" + str(c[4:].split(":")[0]) + "\n"
+                        class_def_found = False
+                        def_found = True
+                        class_found = False
+                    elif c[0:5] == "class":
+                        class_found = True
+                        def_found = False
+                        class_def_found = False
+                        print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                        print_out += "\ttitle: " + str(c[4:].split("(")[0]) + "\n"
+                        print_out += "\tfunction:\n"
+                        print_out += "\tinput:\n"
+                        print_out += "\toutput:\n"
+                        print_out += "\t\'\'\'\n"
+                        print_out += "\t" + str(c[6:].split(":")[0]) + " #class\n"
+            else:
+                if c[0:3] == "def":
+                    def_found = True
+                    class_found = False
+                    print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                    print_out += "\ttitle: " + str(c[4:].split("(")[0]) + "\n"
+                    print_out += "\tfunction:\n"
+                    print_out += "\tinput:\n"
+                    print_out += "\toutput:\n"
+                    print_out += "\t\'\'\'\n"
+                    print_out += "\t" + str(c[4:].split(":")[0]) + "\n"
+                elif c[0:5] == "class":
+                    class_found = True
+                    def_found = False
+                    print_out += "\n\n\t\'\'\'--------------------------------------------------------------------------------------------\n"
+                    print_out += "\ttitle: " + str(c[6:].split(":")[0].split("(")[0]) + "\n"
+                    print_out += "\tfunction:\n"
+                    print_out += "\tinput:\n"
+                    print_out += "\toutput:\n"
+                    print_out += "\t\'\'\'\n"
+                    print_out += "\t" + str(c[6:].split(":")[0]) + " #class\n"
+    print_out += "#--------------------------------------------------------------------------------------------------\n"
+    print_out += "if __name__ == \"__main__\":\n"
+    print_out += "\tmain()\n"
+    
+    out_file = str(options.i.split(".")[0]) + "_defs.py"
+
+    with open(out_file, 'w') as f:
+        f.write(print_out)
+
+
+
+#--------------------------------------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
